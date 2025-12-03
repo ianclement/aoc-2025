6abf49c05fba85fd79a1e7bd5b70b56de9cbaaea
Ian Clement <ian.clement@johabbott.qc.ca>
2025-12-02 22:12:03 -0500

    Day 2 - part 2

--- Day02.lean
+++ Day02.lean
@@ -1,4 +1,5 @@
 import Aoc2025
+import Std.Data.HashSet
 
 -- def substr (s : String) (lower : Nat) (upper : Nat) (h: lower < upper): String :=
 --   let a := s.pos! (String.Pos.Raw.mk lower)
@@ -11,15 +12,11 @@ import Aoc2025
 
 -- def prefix (s : String) (l : Nat) (h : l ≤ s.length) : String :=
   
-def print (r : Std.Range) : String :=
-  s!"{r.start}-{r.stop}"
 
 def forEach (r : Std.Range) (f : Nat → α) [ToString α] : List α  := Id.run do
-  --dbg_trace "Range: {print r}"
   let mut xs := [] 
   for i in r do
      xs := xs.append [f i]
-  --dbg_trace "Result: {xs}"
   return xs
 
 
@@ -42,11 +39,11 @@ def build (digits : Nat) (root : Nat) (reps : Nat) : Nat :=
   | 0 => 0
   | n + 1 => build digits root n * 10 ^ digits + root
 
-def checkForInvalid (range : String × String) (size : Nat) : Nat :=
+def checkForInvalid (range : String × String) (size : Nat) : List Nat :=
   let (lowS, highS) := range
-  dbg_trace "Range:  {lowS} {highS}"
+  dbg_trace "Range:  {lowS} {highS} {size}"
   if lowS.length % size != 0
-  then 0
+  then []
   else
     let lowPrefix := lowS.take size
     let highPrefix := highS.take size
@@ -54,31 +51,31 @@ def checkForInvalid (range : String × String) (size : Nat) : Nat :=
     let ids := forEach [lowPrefix.toNat! : highPrefix.toNat! + 1] (λ n =>
       build size n reps
     )
-    dbg_trace "Ids found: {ids}"
     let check x := lowS.toNat! <= x && x <= highS.toNat!
     let invalidIds := ids.filter check
-    invalidIds.sum
+    dbg_trace "Ids found: {invalidIds}"
+    invalidIds
 
-def isEvenLength (n : Nat) : Bool :=
-  let s := toString n
-  s.length % 2 == 0
+-- def isEvenLength (n : Nat) : Bool :=
+--   let s := toString n
+--   s.length % 2 == 0
 
-def findInvalid (range : String × String) : Int :=
-  let (lowS, highS) := range
-  dbg_trace "Range:  {lowS} {highS}"
-  let halfWidth := (max lowS.length highS.length) / 2
-  let halfLowS := lowS.take (lowS.length - halfWidth)
-  let halfHighS := highS.take (highS.length - halfWidth)
-  let halfLow := halfLowS.toNat!
-  let halfHigh := halfHighS.toNat!
-  let low := lowS.toNat!
-  let high := highS.toNat!
-  let scale := 10 ^ halfWidth -- if halfWidth > 0 then 10 ^ halfWidth else 0
-  dbg_trace "halfLow {halfLow}, halfHigh {halfHigh}, scale {scale}"
-  let ids := forEach [halfLow : halfHigh + 1] (invalid low high scale)
-  let ids' := ids.filter isEvenLength
-  dbg_trace "Ids found: {ids'}"
-  ids'.sum
+-- def findInvalid (range : String × String) : Int :=
+--   let (lowS, highS) := range
+--   dbg_trace "Range:  {lowS} {highS}"
+--   let halfWidth := (max lowS.length highS.length) / 2
+--   let halfLowS := lowS.take (lowS.length - halfWidth)
+--   let halfHighS := highS.take (highS.length - halfWidth)
+--   let halfLow := halfLowS.toNat!
+--   let halfHigh := halfHighS.toNat!
+--   let low := lowS.toNat!
+--   let high := highS.toNat!
+--   let scale := 10 ^ halfWidth -- if halfWidth > 0 then 10 ^ halfWidth else 0
+--   dbg_trace "halfLow {halfLow}, halfHigh {halfHigh}, scale {scale}"
+--   let ids := forEach [halfLow : halfHigh + 1] (invalid low high scale)
+--   let ids' := ids.filter isEvenLength
+--   dbg_trace "Ids found: {ids'}"
+--   ids'.sum
 
 def parseInput (input : String) : List (String × String) :=
   let rawRanges := input.splitOn ","
@@ -97,11 +94,26 @@ def solve1 (input : String) : String :=
   let ranges := parseInput input     
   let splitRanges := ranges.flatMap splitRange
   let splitRanges' := splitRanges.filter (λ r => r.fst.length != 1)  -- just get rid of 1's
-  let totals := splitRanges'.map (λ r => checkForInvalid r (ceilDiv r.fst.length 2)) 
-  toString (totals.sum)
-  --let invalidIds := splitRanges.map findInvalid
-  --toString (invalidIds.sum)
+  let invalidIds := splitRanges'.map (λ r => checkForInvalid r (ceilDiv r.fst.length 2)) 
+  toString invalidIds.flatten.sum
 
-def main : IO Unit := interact solve1
+def checkAllSizes (range : String × String) : List Nat :=
+  let invalidIds := forEach [1 : range.fst.length / 2 + 1] (λ n => checkForInvalid range n)
+  invalidIds.flatten
+
+
+
+def uniq (xs : List α) [BEq α] [Hashable α] : List α := 
+  let set := Std.HashSet.ofList xs
+  set.toList
+
+def solve2 (input : String) : String :=
+  let ranges := parseInput input     
+  let splitRanges := ranges.flatMap splitRange
+  let invalidIds := splitRanges.map checkAllSizes
+  let invalidIds' := uniq (invalidIds.flatten)
+  toString invalidIds'.sum
+
+def main : IO Unit := interact solve2
 
 
