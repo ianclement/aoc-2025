85e1d1c491daeab6f5b5ab61b7fd3c7eba8c2862
Ian Clement <ian.clement@johabbott.qc.ca>
2025-12-02 21:43:08 -0500

    Day 2 refactored

--- Day02.lean
+++ Day02.lean
@@ -23,38 +23,84 @@ def forEach (r : Std.Range) (f : Nat → α) [ToString α] : List α  := Id.run 
   return xs
 
 
+def splitRange (range : String × String) : List (String × String) :=
+  let (low, high) := range
+  if low.length == high.length
+  then [(low, high)]
+  else
+    let midL := String.join (List.replicate low.length "9")
+    let midH := String.join (["1"] ++ List.replicate low.length "0")
+    [(low, midL), (midH, high)]
+
 def invalid (min : Nat) (max: Nat) (s : Nat) (n : Nat) : Nat :=
   let tmp := n * s + n
   if min <= tmp && tmp <= max then tmp else 0
 
 
+def build (digits : Nat) (root : Nat) (reps : Nat) : Nat :=
+  match reps with
+  | 0 => 0
+  | n + 1 => build digits root n * 10 ^ digits + root
+
+def checkForInvalid (range : String × String) (size : Nat) : Nat :=
+  let (lowS, highS) := range
+  dbg_trace "Range:  {lowS} {highS}"
+  if lowS.length % size != 0
+  then 0
+  else
+    let lowPrefix := lowS.take size
+    let highPrefix := highS.take size
+    let reps := lowS.length / size
+    let ids := forEach [lowPrefix.toNat! : highPrefix.toNat! + 1] (λ n =>
+      build size n reps
+    )
+    dbg_trace "Ids found: {ids}"
+    let check x := lowS.toNat! <= x && x <= highS.toNat!
+    let invalidIds := ids.filter check
+    invalidIds.sum
+
 def isEvenLength (n : Nat) : Bool :=
   let s := toString n
   s.length % 2 == 0
 
-def findInvalid (r : String) : Int :=
-  match r.splitOn "-" with
-  | [lowS, highS] =>
-      dbg_trace "Range:  {lowS} {highS}"
-      let halfWidth := (max lowS.length highS.length) / 2
-      let halfLowS := lowS.take (lowS.length - halfWidth)
-      let halfHighS := highS.take (highS.length - halfWidth)
-      let halfLow := halfLowS.toNat!
-      let halfHigh := halfHighS.toNat!
-      let low := lowS.toNat!
-      let high := highS.toNat!
-      let scale := 10 ^ halfWidth -- if halfWidth > 0 then 10 ^ halfWidth else 0
-      dbg_trace "halfLow {halfLow}, halfHigh {halfHigh}, scale {scale}"
-      let ids := forEach [halfLow : halfHigh + 1] (invalid low high scale)
-      let ids' := ids.filter isEvenLength
-      dbg_trace "Ids found: {ids'}"
-      ids'.sum
-  | _ => -1
+def findInvalid (range : String × String) : Int :=
+  let (lowS, highS) := range
+  dbg_trace "Range:  {lowS} {highS}"
+  let halfWidth := (max lowS.length highS.length) / 2
+  let halfLowS := lowS.take (lowS.length - halfWidth)
+  let halfHighS := highS.take (highS.length - halfWidth)
+  let halfLow := halfLowS.toNat!
+  let halfHigh := halfHighS.toNat!
+  let low := lowS.toNat!
+  let high := highS.toNat!
+  let scale := 10 ^ halfWidth -- if halfWidth > 0 then 10 ^ halfWidth else 0
+  dbg_trace "halfLow {halfLow}, halfHigh {halfHigh}, scale {scale}"
+  let ids := forEach [halfLow : halfHigh + 1] (invalid low high scale)
+  let ids' := ids.filter isEvenLength
+  dbg_trace "Ids found: {ids'}"
+  ids'.sum
+
+def parseInput (input : String) : List (String × String) :=
+  let rawRanges := input.splitOn ","
+  rawRanges.map (λ s => 
+    match s.splitOn "-" with
+    | [l, h] => (l, h)
+    | _ => ("", "")  -- TODO should be add error to parsing?
+  )
+
+def ceilDiv (x : Nat) (y : Nat) : Nat :=
+  if x % y == 0
+  then x / y
+  else x / y + 1
 
 def solve1 (input : String) : String :=
-  let ranges := input.splitOn ","
-  let counts := ranges.map findInvalid
-  toString (counts.sum)
+  let ranges := parseInput input     
+  let splitRanges := ranges.flatMap splitRange
+  let splitRanges' := splitRanges.filter (λ r => r.fst.length != 1)  -- just get rid of 1's
+  let totals := splitRanges'.map (λ r => checkForInvalid r (ceilDiv r.fst.length 2)) 
+  toString (totals.sum)
+  --let invalidIds := splitRanges.map findInvalid
+  --toString (invalidIds.sum)
 
 def main : IO Unit := interact solve1
 
